f01fed796c587c1865cc34662eb09e944d814f4e
Igor Martins <igor.rmartins@outlook.com>
2026-06-25 18:36:00 -0300

    Update zsh configuration: modify dotDir path to use home directory variable for improved portability and consistency.

--- nix/home/zsh.nix
+++ nix/home/zsh.nix
@@ -8,7 +8,7 @@
 {
   programs.zsh = {
     enable = true;
-    dotDir = ".config/zsh";
+    dotDir = "${config.home.homeDirectory}/.config/zsh";
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
